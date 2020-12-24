d43880684871ab6743f5325216d617ebfc66fa2b
Vaggelhs <konsvag@gmail.com>
2020-12-24 22:10:30 +0200

    global style component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Routes from "./components/Routes";
+import GlobalStyle from "./globalStyles";
 
 export default function App() {
     return (
         <div>
+            <GlobalStyle />
             <Routes />
         </div>
     );
